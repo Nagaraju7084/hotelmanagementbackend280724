96f2292b37b0c074c419c8e033987aead1b7d45e
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2024-07-28 13:35:08 -0400

    adding all files

---
+++ src/main/java/com/hotel/backend/Hotelmanagementbackend280724Application.java
@@ -0,0 +1,13 @@
+package com.hotel.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hotelmanagementbackend280724Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hotelmanagementbackend280724Application.class, args);
+	}
+
+}
